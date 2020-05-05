70891d346100e63c90c31bc2221e7c6672ff9031
Keerti Kotaru <kvkirthy@gmail.com>
2020-05-05 18:07:38 -0400

    revert to previous state

--- js-modules-demo/JS/create-todo.js
+++
@@ -1,9 +0,0 @@
-export const createTodo = () => {
-    let createTodoElm = "<div>";
-    createTodoElm += "<input type=\"text\" />";
-    createTodoElm += "<button> Create New Todo </button>";
-    createTodoElm += "</div>";
-
-    return createTodoElm;
-
-}
